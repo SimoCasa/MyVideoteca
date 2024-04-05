98a1c19013e9131de6a6a0f0297366151943f7cf
simoc <abbb9642@gmail.com>
2024-04-05 18:15:32 +0200

    Prime modifiche generali

--- app/src/main/java/com/uninsubria/myvideoteca/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/uninsubria/myvideoteca/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Questo è un Fragment home"
     }
     val text: LiveData<String> = _text
 }
